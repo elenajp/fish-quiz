089e3b55edee536b1fc597d09251145a46295dbd
elena perez <elenaperez@ejperezk.home>
2020-12-12 19:11:59 +0100

    did api call to grab data and focus on specific elements

---
+++ fishGuess.py
@@ -0,0 +1,15 @@
+import requests
+import json
+
+url = "https://fishbase.ropensci.org/species"
+
+payload={}
+headers = {}
+
+response = requests.request("GET", url, headers=headers, data=payload)
+fish_data = json.loads(response.text)
+fish_species = fish_data['data'][0]['Species']
+dangerous = fish_data['data'][0]['Dangerous']
+comments = fish_data['data'][0]['Comments']
+image = fish_data['data'][0]['image']
+
